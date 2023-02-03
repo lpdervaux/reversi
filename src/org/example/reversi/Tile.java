e26391fac5a672f57199afe635f0b6d3f353c010
lpdervaux <lp.dervaux@proton.me>
2023-02-03 15:12:21 +0100

    Remove presentation logic from Tile

--- src/org/example/reversi/Tile.java
+++ src/org/example/reversi/Tile.java
@@ -2,41 +2,9 @@ package org.example.reversi;
 
 /**
  * Tiles of reversi.
- * Still has associated code point and {@code String} from early development that are to be removed.
  */
-// TODO: remove all presentation logic from this
 public enum Tile {
-    WHITE("o".codePointAt(0)),
-    BLACK("●".codePointAt(0)),
-    FREE("·".codePointAt(0));
-
-    private final int symbol; // associated code point
-    private final String symbolString; // associated String
-
-    Tile(int symbol) {
-        this.symbol = symbol;
-        this.symbolString = Character.toString(symbol);
-    }
-
-    /**
-     * @return {@code Tile}'s symbol code point
-     */
-    public int symbol() {
-        return symbol;
-    }
-
-    /**
-     * @return {@code Tile}'s symbol {@code String}
-     */
-    public String symbolString() {
-        return symbolString;
-    }
-
-    /**
-     * @return Tile's symbol
-     */
-    @Override
-    public String toString() {
-        return symbolString;
-    }
+    WHITE,
+    BLACK,
+    FREE
 }
